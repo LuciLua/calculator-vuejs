73f864ed42130ca251e8bbff0e6b161a21ba4e18
LuciLua <luci.lua81@gmail.com>
2022-04-03 19:12:37 -0300

    :star2: [483] Criando Projeto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
-    <div id="app">
-        <h1>Calculadora</h1>
-    </div>
+  <div id="app">
+    <h1>Calculadora</h1>
+  </div>
 </template>
-
 <script>
-
-
-export default {
-
-}
+// versao de dependencias nao esta dando certo com as atuais
+export default {};
 </script>
 
 <style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+* {
+  font-family: "RobotoMono";
+}
 
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App'
 
 new Vue({
     render: h => h(App)
+    // é igual a:
+    // render(createElement){
+    //     return createElement(App)
+    // }
 }).$mount("#app")
+//  renderizando componente dntro da div com id app
